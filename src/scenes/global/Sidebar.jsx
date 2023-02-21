1acd992423731eeea613132669551b7b58d5b2a7
Edberaga <edbetjonnathan@gmail.com>
2023-02-22 02:58:57 +0700

    Sidebar index

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,10 @@
+import { React, useState }from 'react'
+import 'react-pro-sidebar/dist/css/styles.css';
+
+const Sidebar = () => {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
