4fe1ee077ef0a8c8ed79ef84abe80bfe061a0c00
Edberaga <edbetjonnathan@gmail.com>
2023-02-23 15:59:11 +0700

    Initial Message page

---
+++ src/scenes/message/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import React from 'react'
+import Header from '../../components/Header';
+
+const Message = () => {
+  return (
+    <Box m="20px">
+      <Header title="MESSAGE" subtitle="Communicate with Teams and Clients" />
+    </Box>
+  )
+}
+
+export default Message;
