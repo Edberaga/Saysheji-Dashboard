373432d995b6a067247393509789ee09a82cefc8
Edberaga <edbetjonnathan@gmail.com>
2023-02-23 15:59:45 +0700

    Initialize Team page

---
+++ src/scenes/team/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import React from 'react'
+import Header from '../../components/Header';
+
+const Team = () => {
+  return (
+    <Box m="20px">
+      <Header title="TEAMS" subtitle="View your Workplace teams here and they current task" />
+    </Box>
+  )
+}
+
+export default Team;
