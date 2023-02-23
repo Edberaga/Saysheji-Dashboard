6afadde630b33b0f7c0469b791cc198c55686e29
Edberaga <edbetjonnathan@gmail.com>
2023-02-23 15:57:50 +0700

    Initial calendar page

---
+++ src/scenes/calendar/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import React from 'react'
+import Header from '../../components/Header';
+
+const Calendar = () => {
+  return (
+    <Box m="20px">
+      <Header title="CALENDAR" subtitle="View or Manage your Work Timetable" />
+    </Box>
+  )
+}
+
+export default Calendar;
