f43891308895d74c13334ce562fa4e6c7bd696a8
Edberaga <edbetjonnathan@gmail.com>
2023-02-23 15:57:40 +0700

    Initial dashboard page

---
+++ src/scenes/board/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import React from 'react'
+import Header from '../../components/Header';
+
+const Board = () => {
+  return (
+    <Box m="20px">
+      <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+    </Box>
+  )
+}
+
+export default Board;
