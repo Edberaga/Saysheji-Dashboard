208fdfff3e96fb8d05b3a37b21ae84bcb10b964b
Edberaga <edbetjonnathan@gmail.com>
2023-02-23 15:59:27 +0700

    Initiate Project page

---
+++ src/scenes/project/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import React from 'react'
+import Header from '../../components/Header';
+
+const Project = () => {
+  return (
+    <Box m="20px">
+      <Header title="PROJECT" subtitle="View your Projects and Tasks List" />
+    </Box>
+  )
+}
+
+export default Project
