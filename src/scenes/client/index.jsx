4f6c9492d3a883f523fb7407f8d3dec6a2cc1470
Edberaga <edbetjonnathan@gmail.com>
2023-02-23 15:58:02 +0700

    initial client webpage

---
+++ src/scenes/client/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import React from 'react'
+import Header from '../../components/Header';
+
+const Client = () => {
+  return (
+    <Box m="20px">
+      <Header title="CLIENTS" subtitle="View and Manage Clients" />
+    </Box>
+  )
+}
+
+export default Client;
