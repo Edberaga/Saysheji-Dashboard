ee7dcef9a70897736ed24ea6f555055dc12df34e
Edberaga <edbetjonnathan@gmail.com>
2023-02-23 15:56:07 +0700

    Add the rest webpage link

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import { ColorModeContext, useMode } from './theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { Routes, Route } from 'react-router-dom';
 import './index.css'
+
+
+//Link Webpage for Router
+import Board from './scenes/board';
+import Project from './scenes/project';
+import Team from './scenes/team';
+import Client from './scenes/client';
+import Message from './scenes/message';
+import Calendar from './scenes/calendar';
+
+//Global components
+import Sidebar from './scenes/global/Sidebar';
 import { Topbar } from './scenes/global/Topbar';
-import { Dashboard } from '@mui/icons-material';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -15,11 +26,17 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
 
-        <div className="App">
+        <div className="app">
+          <Sidebar/>
           <main className="content">
             <Topbar/>
             <Routes>
-              
+              <Route path='/' element={<Board/>}/>
+              <Route path='/project' element={<Project/>}/>
+              <Route path='/team' element={<Team/>}/>
+              <Route path='/client' element={<Client/>}/>
+              <Route path='/message' element={<Message/>}/>
+              <Route path='/calendar' element={<Calendar/>}/>
             </Routes>
           </main>
         </div>
