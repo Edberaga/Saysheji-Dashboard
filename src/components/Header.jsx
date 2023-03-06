a0f9b7d789e4ffd241bdf11059dcb379924a5f16
Edberaga <edbetjonnathan@gmail.com>
2023-03-06 17:01:15 +0700

    Add flexbox and button for adding new user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,52 @@
 import React from 'react'
-import { Typography, Box, useTheme } from '@mui/material';
+import { Typography, Box, useTheme, Button } from '@mui/material';
 import { tokens } from '../theme';
+import { padding } from '@mui/system';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
-const Header = ({ title, subtitle }) => {
+const Header = ({ title, subtitle, button }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   
   return (
     <Box mb="20px">
-        <Typography 
-        variant="h2"
-        color={colors.grey[100]}
-        fontWeight="bold"
-        sx={{ mb: "5px" }}
-        >
-            {title}
-        </Typography>
-        <Typography
-        variant="h5"
-        color={colors.blueAccent[400]}
-        >
-            {subtitle}
-        </Typography>
+      <Box style={{ 
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}>
+        <Box>
+          <Typography 
+          variant="h2"
+          color={colors.grey[100]}
+          fontWeight="bold"
+          sx={{ mb: "5px" }}
+          >
+              {title}
+          </Typography>
+          <Typography
+          variant="h5"
+          color={colors.blueAccent[400]}
+          >
+              {subtitle}
+          </Typography>
+        </Box>
+        
+        
+        <Box>
+          <Button
+            variant="contained" 
+            style={{
+              color: colors.blueAccent[300],
+              border: `2px solid ${colors.blueAccent[300]}`,
+              padding: '5px 15px',
+            }}
+            startIcon={<PersonAddIcon/>}
+          >
+            {button}
+          </Button>
+        </Box>
+      </Box>
     </Box>
 
   )
