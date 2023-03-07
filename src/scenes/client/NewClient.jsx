94b34b2b5e54c70ae57d4282fc5575cb50fe5706
Edberaga <edbetjonnathan@gmail.com>
2023-03-08 04:00:37 +0700

    Initialize Client form page

---
+++ src/scenes/client/NewClient.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+const NewClient = () => {
+  return (
+    <Header title='Add New Client'/>
+  )
+}
+
+export default NewClient
