632ed0ec708bacf1fea6c616bf5a8d6a6db8267d
Edberaga <edbetjonnathan@gmail.com>
2023-09-06 23:46:46 +0700

    Restructure main code and set the Topbar again

--- src/App.jsx
+++ src/App.jsx
@@ -24,47 +24,50 @@ import Login from './scenes/login';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebase';
 import Sidebar from './scenes/global/Sidebar';
+import { Topbar } from './scenes/global/Topbar';
 
 function App() {
   const [theme, colorMode] = useMode();
   const [user] = useAuthState(auth);
 
   return (
+<>
+<ColorModeContext.Provider value={colorMode}>
+  <ThemeProvider theme={theme}>
+    {user ?
     <>
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="app">
-          {user ?
-          <>
-          <Sidebar/>
-          <Routes>
-            <Route path='/' element={<Board/>}/>
-            <Route path='/project' element={<Project/>}/>
-            <Route path='/team'>
-              <Route index element={<Team/>}/>
-              <Route path=':teamId' element={<SingleTeam/>}/>
-              <Route path='newTeam' element={<NewTeam/>}/>
-            </Route>
-            <Route path='/client' >
-              <Route index element={<Client/>}/>
-              <Route path=':clientId' element={<SingleClient/>}/>
-              <Route path='newClient' element={<NewClient/>}/>
-            </Route>
-            <Route path='/message' element={<Message/>}/>
-            <Route path='/calendar' element={ <Calendar/>}/>
-          </Routes>
-          </>
-          : 
-          <Routes>
-            <Route path='/' element={<Login/>}/>
-          </Routes>
-          }
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <CssBaseline />
+    <div className="app">
+    <Sidebar/>
+    <main className="content">
+    <Topbar/>
+      <Routes>
+        <Route path='/' element={<Board/>}/>
+        <Route path='/project' element={<Project/>}/>
+        <Route path='/team'>
+          <Route index element={<Team/>}/>
+          <Route path=':teamId' element={<SingleTeam/>}/>
+          <Route path='newTeam' element={<NewTeam/>}/>
+        </Route>
+        <Route path='/client' >
+          <Route index element={<Client/>}/>
+          <Route path=':clientId' element={<SingleClient/>}/>
+          <Route path='newClient' element={<NewClient/>}/>
+        </Route>
+        <Route path='/message' element={<Message/>}/>
+        <Route path='/calendar' element={ <Calendar/>}/>
+      </Routes>
+    </main>
+    </div>
     </>
-  )
-}
+    : 
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+      </Routes>
+    }
+  </ThemeProvider>
+</ColorModeContext.Provider>
+</>
+)}
 
 export default App
