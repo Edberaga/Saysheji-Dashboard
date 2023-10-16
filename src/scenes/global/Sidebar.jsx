697b0ba44c702494bae55e6e5d5982bd25f81cbc
Edberaga <edbetjonnathan@gmail.com>
2023-10-16 23:55:54 +0700

    use id instead

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -46,7 +46,7 @@ const Sidebar = () => {
   useEffect(() => {
     const getUserData = async() => {
       try{
-        const docRef = doc(db, "users", user.email);
+        const docRef = doc(db, "users", user.uid);
         const docSnap = await getDoc(docRef);
         setData(docSnap.data());
       }
@@ -71,174 +71,174 @@ const Sidebar = () => {
   const [selected, setSelected] = useState(); //Determine what page we select and currently at. (Dashboard) as the default
   
   return (
-    <Box
-      sx={{
-        "& .pro-sidebar-inner": {
-          background: `${colors.primary[500]} !important`
-        },
-        "& .pro-icon-wrapper": {
-          backgroundColor: "transparent !important"
-        },
-        "& .pro-inner-item" : {
-          padding: "5px 35px 5px 20px !important"
-        },
-        "& .pro-inner-item:hover": {
-          color: `${colors.blueAccent[400]} !important`,
-        },
-        "& .pro-menu-item.active": {
-          color: `${colors.blueAccent[500]} !important`,
-        },
-      }}
-    >
-      <ProSidebar collapsed={isCollapsed}>
-        <Menu iconShape='square'>
-          {/*LOGO AND MENU ICON */}
-          <MenuItem
-            onClick={() => setIsCollapsed(!isCollapsed)}
-            icon={isCollapsed ? <MenuOutlinedIcon/> : undefined}
-            style={{
-              margin: "10px 0 20px 0",
-              color: colors.grey[100],
-            }}
+  <Box
+    sx={{
+      "& .pro-sidebar-inner": {
+        background: `${colors.primary[500]} !important`
+      },
+      "& .pro-icon-wrapper": {
+        backgroundColor: "transparent !important"
+      },
+      "& .pro-inner-item" : {
+        padding: "5px 35px 5px 20px !important"
+      },
+      "& .pro-inner-item:hover": {
+        color: `${colors.blueAccent[400]} !important`,
+      },
+      "& .pro-menu-item.active": {
+        color: `${colors.blueAccent[500]} !important`,
+      },
+    }}
+  >
+    <ProSidebar collapsed={isCollapsed}>
+      <Menu iconShape='square'>
+        {/*LOGO AND MENU ICON */}
+        <MenuItem
+          onClick={() => setIsCollapsed(!isCollapsed)}
+          icon={isCollapsed ? <MenuOutlinedIcon/> : undefined}
+          style={{
+            margin: "10px 0 20px 0",
+            color: colors.grey[100],
+          }}
+        >
+        {!isCollapsed && (
+        <Box
+          display="flex"
+          justifyContent="flex-end"
+          alignItems="center"
+          ml="15px"
+        >
+          <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+            <MenuOutlinedIcon/>
+          </IconButton>
+        </Box>
+        )}
+      </MenuItem>
+
+      {/* USER */}
+      {!isCollapsed && (
+      <Box mb="25px">
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <Box
+          alt="profile-user"
+          className="sidebar-box"
+          width="100px"
+          height="100px"
+          style={{
+            cursor: "pointer", 
+            borderRadius: "50%",
+            backgroundImage: `url(${data.img})`,
+            backgroundSize: 'cover',
+          }}
+          />
+        </Box>
+
+        <Box textAlign="center">
+        {user && (
+          <>
+          <Typography
+            variant='h6'
+            color={colors.grey[100]}
+            fontWeight="400"
+            sx={{ m: "10px 0 0 0"}}
           >
-            {!isCollapsed && (
-              <Box
-                display="flex"
-                justifyContent="flex-end"
-                alignItems="center"
-                ml="15px"
-              >
-                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
-                  <MenuOutlinedIcon/>
-                </IconButton>
-              </Box>
-            )}
-          </MenuItem>
-
-          {/* USER */}
-          {!isCollapsed && (
-            <Box mb="25px">
-              <Box display="flex" justifyContent="center" alignItems="center">
-                <Box
-                alt="profile-user"
-                className="sidebar-box"
-                width="100px"
-                height="100px"
-                style={{
-                  cursor: "pointer", 
-                  borderRadius: "50%",
-                  backgroundImage: `url(${data.img})`,
-                  backgroundSize: 'cover',
-                }}
-                />
-              </Box>
-
-              <Box textAlign="center">
-              {user && (
-                <>
-                <Typography
-                  variant='h6'
-                  color={colors.grey[100]}
-                  fontWeight="400"
-                  sx={{ m: "10px 0 0 0"}}
-                >
-                  {data.username}
-                </Typography>
-                <Typography 
-                  variant='h6' 
-                  color={colors.blueAccent[500]}
-                >
-                  {data.position}
-                </Typography>
-                </>
-              )}
-              </Box>
-            </Box>
-          )}
-
-          {/* MENU ITEMS */}
-          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
-            <Typography
-              variant='h6'
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
-              Main
-            </Typography>
-
-            <Item 
-              title="Dashboard"
-              to="/"
-              icon={<DashboardOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-
-            <Typography
-              variant='h6'
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
-              Lists
-            </Typography>
-            <Item 
-              title="Project"
-              to="/project"
-              icon={<AssignmentOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item 
-              title="Teams"
-              to="/team"
-              icon={<AssignmentIndOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item 
-              title="Clients"
-              to="/client"
-              icon={<AccountBoxOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            
-            <Typography
-              variant='h6'
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
-              Service
-            </Typography>
-
-            <Item 
-              title="Message"
-              to="/message"
-              icon={<ForumOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item 
-              title="Calendar"
-              to="/calendar"
-              icon={<CalendarMonthOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-          </Box>
-          <Box 
-            style={{
-              cursor: "pointer",
-              margin: "5% 0 0 15%",
-              color: "red"
-            }}
-            onClick={logOut}
+            {data.username}
+          </Typography>
+          <Typography 
+            variant='h6' 
+            color={colors.blueAccent[500]}
           >
-          Logout
-          </Box>
-        </Menu>
-      </ProSidebar>
-    </Box>
+            {data.position}
+          </Typography>
+          </>
+        )}
+        </Box>
+      </Box>
+      )}
+
+      {/* MENU ITEMS */}
+      <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+        <Typography
+          variant='h6'
+          color={colors.grey[300]}
+          sx={{ m: "15px 0 5px 20px" }}
+        >
+          Main
+        </Typography>
+
+        <Item 
+          title="Dashboard"
+          to="/"
+          icon={<DashboardOutlinedIcon />}
+          selected={selected}
+          setSelected={setSelected}
+        />
+
+        <Typography
+          variant='h6'
+          color={colors.grey[300]}
+          sx={{ m: "15px 0 5px 20px" }}
+        >
+          Lists
+        </Typography>
+        <Item 
+          title="Project"
+          to="/project"
+          icon={<AssignmentOutlinedIcon />}
+          selected={selected}
+          setSelected={setSelected}
+        />
+        <Item 
+          title="Teams"
+          to="/team"
+          icon={<AssignmentIndOutlinedIcon />}
+          selected={selected}
+          setSelected={setSelected}
+        />
+        <Item 
+          title="Clients"
+          to="/client"
+          icon={<AccountBoxOutlinedIcon />}
+          selected={selected}
+          setSelected={setSelected}
+        />
+        
+        <Typography
+          variant='h6'
+          color={colors.grey[300]}
+          sx={{ m: "15px 0 5px 20px" }}
+        >
+          Service
+        </Typography>
+
+        <Item 
+          title="Message"
+          to="/message"
+          icon={<ForumOutlinedIcon />}
+          selected={selected}
+          setSelected={setSelected}
+        />
+        <Item 
+          title="Calendar"
+          to="/calendar"
+          icon={<CalendarMonthOutlinedIcon />}
+          selected={selected}
+          setSelected={setSelected}
+        />
+      </Box>
+      <Box 
+        style={{
+          cursor: "pointer",
+          margin: "5% 0 0 15%",
+          color: "red"
+        }}
+        onClick={logOut}
+      >
+      Logout
+      </Box>
+      </Menu>
+    </ProSidebar>
+  </Box>
   );
 };
 
