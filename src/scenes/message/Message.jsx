c3bc46cb3662f6c58f0cab0278b051d9a7269a2e
Edberaga <edbetjonnathan@gmail.com>
2023-11-02 21:52:48 +0700

    Message container

--- src/scenes/message/Message.jsx
+++ src/scenes/message/Message.jsx
@@ -1,6 +1,7 @@
-import { Box, useTheme } from '@mui/material'
+import { Box, Typography, useTheme } from '@mui/material'
 import React from 'react'
 import { tokens } from '../../theme';
+import profilePic from '../../assets/img/user-profile.png'
 import './styles.scss'
 
 const Message = () => {
@@ -8,14 +9,60 @@ const Message = () => {
   const colors = tokens(theme.palette.mode);
   return (
   <>
-  <Box sx={{
-    backgroundColor: colors.blueAccent[800],
-    padding: "10px",
-    margin: "0px 5vw 10px 15px",
-    borderRadius: "2.5px",
-    display: "flex",
-  }}>
-      Message
+  <Box className='message'>
+    <Box className="messageInfo">
+    <img
+      style={{
+        width: "50px", height: "50px",
+        borderRadius: "50%"
+      }}
+      src={profilePic} 
+      alt=""
+    />
+    </Box>
+    <Box 
+      className="messageContent"
+      sx={{
+        backgroundColor: colors.blueAccent[700]
+      }}
+    >
+      <p>Message</p>
+      <Typography 
+        variant='span' 
+        style={{
+        textAlign: "right"
+      }}>
+        Just now
+      </Typography>
+    </Box>
+  </Box>
+
+  <Box className='message owner'>
+    <Box className="messageInfo">
+    <img
+      style={{
+        width: "50px", height: "50px",
+        borderRadius: "50%"
+      }}
+      src={profilePic} 
+      alt=""
+    />
+    </Box>
+    <Box 
+      className="messageContent"
+      sx={{
+        backgroundColor: colors.primary[900]
+      }}
+    >
+      <img src={profilePic} alt="" />
+      <Typography 
+        variant='span' 
+        style={{
+        textAlign: "right"
+      }}>
+        Just now
+      </Typography>
+    </Box>
   </Box>
   </>
   )
